60cea567616f35a13199b98c97b904bb00ab251d
Andrew <awcrotwell@gmail.com>
2020-04-05 10:12:37 -0700

    CI

--- spec/models/user_spec.cr
+++ spec/models/user_spec.cr
@@ -5,11 +5,11 @@ describe User do
   it "should create a new top level user" do
     email = "user_spec_01#{rand(1..100)}@gmail.com"
     user = User.new_top_level({
-      "email" => email,
-      "password" => "Password123",
-      "firstName" => "igni",
-      "lastName" => "base",
-      "termsOfService" => true
+      "email"          => email,
+      "password"       => "Password123",
+      "firstName"      => "igni",
+      "lastName"       => "base",
+      "termsOfService" => true,
     })
 
     user.save.should eq(true)
